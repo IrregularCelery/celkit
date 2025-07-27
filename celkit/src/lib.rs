c583556108a571a57207940e07c05e9473271775
Mohsen Rad <irregular.celery@gmail.com>
2025-07-27 20:27:45 +0330

    Add re-exports and features of the `celkit-string` for the `celkit`

--- celkit/src/lib.rs
+++ celkit/src/lib.rs
@@ -5,3 +5,6 @@ pub mod traits {
 pub mod macros {
     pub use celkit_core::impl_for_struct;
 }
+
+#[cfg(feature = "string")]
+pub use celkit_string::{from_string, to_string};
--- celkit_string/src/lib.rs
+++ celkit_string/src/lib.rs
@@ -1,3 +1,7 @@
-pub fn to_string() {}
+pub fn to_string() {
+    assert!(false, "`to_string()` is not implemented yet!")
+}
 
-pub fn from_string() {}
+pub fn from_string() {
+    assert!(false, "`from_string()` is not implemented yet!")
+}
