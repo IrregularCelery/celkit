f8584d057eec9b8937036ed84359cc5214f74c4e
Mohsen Rad <irregular.celery@gmail.com>
2025-08-16 07:52:46 +0330

    [celkit-string]: Change `Token` implementation to a macro

--- celkit_string/src/common.rs
+++ celkit_string/src/common.rs
@@ -1,6 +1,40 @@
 use celkit_core::internal::sys::*;
 use celkit_core::internal::Number;
 
+macro_rules! impl_for_token {
+    ($($variant:ident => $char:literal),* $(,)?) => {
+        impl Token {
+            pub fn to_char(&self) -> Option<char> {
+                match self {
+                    $(Token::$variant => Some($char),)*
+                    _ => None,
+                }
+            }
+
+            pub fn from_char(c: char) -> Option<Token> {
+                match c {
+                    $($char => Some(Token::$variant),)*
+                    _ => None,
+                }
+            }
+        }
+
+        impl core::fmt::Display for Token {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                match self {
+                    $(Token::$variant => core::write!(f, "{}", $char),)*
+                    Token::Literal(l) => core::write!(f, "\"{}\"", l),
+                    Token::Numeric(n) => core::write!(f, "{}", n),
+                    Token::Boolean(b) => core::write!(f, "{}", b),
+                    Token::Identifier(i) => core::write!(f, "{}", i),
+                    Token::Null => core::write!(f, "null"),
+                    Token::Eof => core::write!(f, ""),
+                }
+            }
+        }
+    }
+}
+
 #[derive(PartialEq)]
 pub(crate) enum Token {
     StructMarker,       // @
@@ -21,48 +55,15 @@ pub(crate) enum Token {
     Eof,                // End of input
 }
 
-impl Token {
-    pub fn get_char(&self) -> Option<char> {
-        match self {
-            Token::StructMarker => Some('@'),
-            Token::ArrayOpen => Some('['),
-            Token::ArrayClose => Some(']'),
-            Token::TupleOpen => Some('('),
-            Token::TupleClose => Some(')'),
-            Token::ObjectOpen => Some('{'),
-            Token::ObjectClose => Some('}'),
-            Token::FieldAssign => Some('='),
-            Token::KeyAssign => Some(':'),
-            Token::Separator => Some(','),
-            Token::Literal(_) => None,
-            Token::Numeric(_) => None,
-            Token::Boolean(_) => None,
-            Token::Identifier(_) => None,
-            Token::Null => None,
-            Token::Eof => None,
-        }
-    }
-}
-
-impl core::fmt::Display for Token {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            Token::StructMarker => core::write!(f, "@"),
-            Token::ArrayOpen => core::write!(f, "["),
-            Token::ArrayClose => core::write!(f, "]"),
-            Token::TupleOpen => core::write!(f, "("),
-            Token::TupleClose => core::write!(f, ")"),
-            Token::ObjectOpen => core::write!(f, "{{"),
-            Token::ObjectClose => core::write!(f, "}}"),
-            Token::FieldAssign => core::write!(f, "="),
-            Token::KeyAssign => core::write!(f, ":"),
-            Token::Separator => core::write!(f, ","),
-            Token::Literal(l) => core::write!(f, "\"{}\"", l),
-            Token::Numeric(n) => core::write!(f, "{}", n),
-            Token::Boolean(b) => core::write!(f, "{}", b),
-            Token::Identifier(i) => core::write!(f, "{}", i),
-            Token::Null => core::write!(f, "null"),
-            Token::Eof => core::write!(f, ""),
-        }
-    }
+impl_for_token! {
+    StructMarker => '@',
+    ArrayOpen => '[',
+    ArrayClose => ']',
+    TupleOpen => '(',
+    TupleClose => ')',
+    ObjectOpen => '{',
+    ObjectClose => '}',
+    FieldAssign => '=',
+    KeyAssign => ':',
+    Separator => ',',
 }
